9b0e95d3da7637c6e77827ed505535de8945f212
Samuel <jread912@gmail.com>
2018-08-29 07:39:31 -0700

    Updated Code In The admin.js File

--- routes/admin.js
+++ routes/admin.js
@@ -408,7 +408,7 @@ routes.post("/add/students/:token", ensureAdminAuthentication, (req, res) => {
                   if(err.response){
                     console.log(err.response.data.errorMsg)
                     req.flash("error_msg", err.response.data.errorMsg);
-                    res.redirect(`/admin/add/student/${token}`);
+                    return res.redirect(`/admin/add/student/${token}`);
                   }
                 });   
             }
